e620dec99a3cc0f1d050216d3c7ab03280f69ea8
Shantari <shantariextanu@googlemail.com>
2016-06-03 14:04:12 +0100

    Added ActionBarDetector class to BarDetector.py (may need refactoring out later)
    Allows detection of weapons on the action bar
    
    TODO:
    
       - Get images for all weapons in the game
       - Find a solution for resolutions whose action bar slots are smaller than 64x64
    
    Also edited the getGameScreenImg() function to take boundary arguments in the form of width/height percentages. Reasoning is explained in comments.

--- client/BarDetector.py
+++ client/BarDetector.py
@@ -5,7 +5,14 @@ import os, win32gui, win32ui, win32con, cv2, time, math, json
 def getKey(item):
     return item[1]
 
-def getGameScreenImg(hwnd):
+# Returns a PIL Image containing a screenshot of a region specified by left, top, right, bottom.
+# left, top, right, bottom. are specified as a float between 0 and 1 which specify a percentage of
+# the maximum screen coordinates.
+#
+# This might seem like primo autism but it's the most convenient way I could think of in 20 seconds
+# to keep this resolution agnostic.
+
+def getGameScreenImg(hwnd, left, top, right, bottom):
     # http://stackoverflow.com/questions/3260559/how-to-get-a-window-or-fullscreen-screenshot-in-python-3k-without-pil
     clientOrigin = win32gui.ClientToScreen(hwnd, (0, 0))
     windowRect = win32gui.GetWindowRect(hwnd)
@@ -37,7 +44,7 @@ def getGameScreenImg(hwnd):
     screenImg = screenImg.crop((clientOrigin[0] - windowRect[0], clientOrigin[1] - windowRect[1], clientRect[2] + (clientOrigin[0] - windowRect[0]), clientRect[3] + (clientOrigin[1] - windowRect[1])))
     width, height = screenImg.size
     #crop the bottom right quarter of the screen
-    screenImg = screenImg.crop((int(width * 0.5), int(height * 0.5), width, height))
+    screenImg = screenImg.crop((int(width*left), int(height*top), width*right, height*bottom))
     return screenImg
     
 def convertToOpenCV(pilImage):
@@ -45,7 +52,7 @@ def convertToOpenCV(pilImage):
 
 def getContours(image, limits):
     shapeMask = cv2.inRange(image, limits[0], limits[1])
-    (cnts, _) = cv2.findContours(shapeMask.copy(), cv2.RETR_EXTERNAL,
+    im, cnts, _ = cv2.findContours(shapeMask.copy(), cv2.RETR_EXTERNAL,
     cv2.CHAIN_APPROX_SIMPLE)
     return cnts
     
@@ -115,7 +122,7 @@ class BarDetector:
         if len(currentHP) == 0 or len(currentThirst) == 0 or len(currentHunger) == 0:
             return
         currentHP, currentThirst, currentHunger = int(currentHP), int(currentThirst), int(currentHunger)
-        screenImg = convertToOpenCV((getGameScreenImg(rustWindow)))
+        screenImg = convertToOpenCV((getGameScreenImg(rustWindow, 0.5, 0.5, 1.0, 1.0)))
         self.approximateBoundingBoxes(self.getBarContours(screenImg))
         #Estimates the length of a full bar if currentHP != 100, if it does then
         #the length measured from detection is accurate
@@ -154,7 +161,7 @@ class BarDetector:
     #Assuming detection has already been properly calibrated, we only have to look in the
     #bounding box
     def findBarBoundingBoxes(self, rustWindow):
-        screenImg = getGameScreenImg(rustWindow)
+        screenImg = getGameScreenImg(rustWindow, 0.5, 0.5, 1.0, 1.0)
         self.hpBarImg = screenImg.crop(calcCoords(self.calibratedBoundingBoxes[0]))
         self.thirstBarImg = screenImg.crop(calcCoords(self.calibratedBoundingBoxes[1]))
         self.hungerBarImg = screenImg.crop(calcCoords(self.calibratedBoundingBoxes[2]))
@@ -237,4 +244,92 @@ class BarDetector:
     def getBarContours(self, image):
         return (getContours(image, self.hpLimits), getContours(image, self.thirstLimits), getContours(image, self.hungerLimits))
         
+class ActionBarDetector:
+
+    # TODO:
+    #
+    #   - Get images for all weapons in the game
+    #   - Find a solution for resolutions whose action bar slots are smaller than 64x64
+    #
+    # Example usage:
+    #
+    # abd = ActionBarDetector()
+    # rustWindow = win32gui.FindWindow(None, 'Rust')
+    # abs = abd.getActionBarSlots(rustWindow)
+    # for slot in abs:
+    #      if MatchImage('bolt.jpg',0.3,slot):
+    #           stats['bolt'] = True
+    # 
+    # and so on.
+    #
+    # Against a reasonably uniform background (rocks, sand, etc.) the templates are matched
+    # to a certainty between 0.3 and 0.5. This ~should~ be enough to differentiate every item.
+    # The only way to be sure is to go through and hand test each one. So far bolt and AK have
+    # passed my tests, although both will fail against a very complicated background, (barbed wire,
+    # etc.). The bolt has been the hardest template to match in the past, so this makes me cofident
+    # this iteration is pretty solid.
+     
+    durabilityLimits = [np.array([50, 100, 100]), np.array([65, 255, 255])] # HSV boundaries
     
+    # Matches supplied template, with name imagename, on image, img, and returns true
+    # if template is matched with a confidence value greater than the threshold.
+    def MatchImage(self, imagename, threshold, img):
+    
+        # Apply canny edge detecting algorithm to image and template
+        # this makes image recognition a lot easier with a noisy background on image
+        # and a white background on the template
+    
+        cannythresh1 = 100
+        cannythresh2 = 200
+        templ = cv2.imread(imagename, cv2.IMREAD_COLOR)
+        templ_edge = cv2.Canny(templ,cannythresh1, cannythresh2)
+        img_edge = cv2.Canny(img,cannythresh1,  cannythresh2)
+        method = eval('cv2.TM_CCOEFF_NORMED')
+        
+        res = cv2.matchTemplate(img_edge, templ_edge, method)
+        
+        #w,h=cv2.cvtColor(templ,cv2.COLOR_BGR2GRAY).shape[::-1]
+        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+        
+        #cv2.rectangle(img, max_loc,(max_loc[0]+w,max_loc[1]+h),255,2)
+        
+        if max_val > threshold:
+            return True
+        else:
+            return False
+        
+    def getDurabilityContours(self, image):
+        return getContours(image, self.durabilityLimits)
+
+    def getActionBarSlots(self, rustWindow):
+    
+        # Tries to find the action bar slots with these assumptions:
+        #
+        # The durability bar is the thinnest bar matching this color contour
+        # All bars are always uniform in width
+        # Durability bars are 1/15th the width of the action bar slot
+        
+        # Returns images of the action bar slots
+        
+        img = convertToOpenCV((getGameScreenImg(rustWindow, 0.25, 0.75, 0.75, 1.0)))
+        durabilityContours = self.getDurabilityContours(img)
+        durabilityBarBoundingBoxes = []
+        actionBarSlotBoundingBoxes = []
+        actionBarSlotImages = []
+        
+        for cnt in durabilityContours:
+            #thirst bar is a rectangle, len(approx) is the number of edges
+            approx = cv2.approxPolyDP(cnt, 0.01 * cv2.arcLength(cnt,True),True)
+            if len(approx) >= 3:
+                durabilityBarBoundingBoxes.append(cv2.boundingRect(cnt))
+                
+        for box in durabilityBarBoundingBoxes:
+            y1 = ((box[1]+box[3])-box[2]*15)
+            y2 = (box[1]+box[3])
+            x1 = box[0]
+            x2 = x1+box[2]*15
+            actionBarSlotImages.append(img[y1:y2,x1:x2])
+            
+        return actionBarSlotImages
+            
+        
