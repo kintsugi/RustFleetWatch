d45417cf44037d375f91d8fd1455ca54dc191fb8
Shantari <shantariextanu@googlemail.com>
2016-06-04 01:37:40 +0100

    Edited setup.py to exclude some windows libraries

--- client/setup.py
+++ client/setup.py
@@ -1,4 +1,12 @@
 from distutils.core import setup
 import py2exe
+opts = {"py2exe": {
+"dll_excludes": ["MSVFW32.dll",
+                 "AVIFIL32.dll",
+                 "AVICAP32.dll",
+                 "ADVAPI32.dll",
+                 "CRYPT32.dll",
+                 "WLDAP32.dll"]
+}}
 
-setup(windows=['RustFleetWatch.py'])
+setup(options=opts,windows=['RustFleetWatch.py'])
